106694bad31303ea1f91b41abbcc0ee875b3b17e
John Borrego <johnborrego36@gmail.com>
2026-01-29 22:35:34 -0500

    Added final changes to Nasa's astronomy picture of the day tab, added image explanation in a caption

---
+++ apis.py
@@ -0,0 +1,21 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+unique_key = os.getenv("NASA_API_KEY")
+
+url= "https://api.nasa.gov/planetary/apod?api_key="
+
+
+
+
+def apod_generator(url,unique_key):
+    final_url = url + unique_key
+    response = requests.get(final_url).json()
+    return response
+
+
+
+
